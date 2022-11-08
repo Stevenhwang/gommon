f2ec8dd0fc676cf56589bcdb9978f8ac4d74ea88
zed <90huanw@gmail.com>
2022-11-08 19:54:02 +0800

    ssh shell

---
+++ ssh/shell.go
@@ -0,0 +1,19 @@
+package ssh
+
+import gossh "golang.org/x/crypto/ssh"
+
+func SSHExec(cmd string, client *gossh.Client) (string, error) {
+	defer client.Close()
+	//创建session
+	session, err := client.NewSession()
+	if err != nil {
+		return "", err
+	}
+	defer session.Close()
+	//执行远程命令
+	combo, err := session.CombinedOutput(cmd)
+	if err != nil {
+		return "", err
+	}
+	return string(combo), nil
+}
