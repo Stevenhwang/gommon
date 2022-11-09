d0246b3289bffa326d495d33f54f8c904b27faf3
zed <90huanw@gmail.com>
2022-11-09 16:00:31 +0800

    fix

--- ssh/shell.go
+++ ssh/shell.go
@@ -4,14 +4,14 @@ import gossh "golang.org/x/crypto/ssh"
 
 // SSHExec 执行 ssh shell 远程命令
 func SSHExec(cmd string, client *gossh.Client) (string, error) {
-	defer client.Close()
-	//创建session
+	// defer client.Close()
+	// 创建session
 	session, err := client.NewSession()
 	if err != nil {
 		return "", err
 	}
 	defer session.Close()
-	//执行远程命令
+	// 执行远程命令
 	combo, err := session.CombinedOutput(cmd)
 	if err != nil {
 		return "", err
